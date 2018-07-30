5fa7c76360e27365294a45b7a557be2d6fd6d0df
Dee Clawson <kdclaw3@gmail.com>
2018-07-30 15:34:10 -0400

    v1.0.3
    -earthrocker linting

--- index.js
+++ index.js
@@ -2,60 +2,60 @@ const crypto = require('crypto');
 
 module.exports = {
 
-	match: (un, ipw, pw, iv = '0123456789ABCDEF') => {
+  match: (un, ipw, pw, iv = '0123456789ABCDEF') => {
 
-		//function validation
-		let inputs = [un, ipw, pw, iv];
-		let validateFalse = false;
-		for (let i in inputs) {
-			if (typeof inputs[i] !== 'string' || inputs[i] === '') {
-				let n = Number(i)+1, s=['th', 'st', 'nd', 'rd'], v=n%100;
-				console.error(`[RAM ORACLE] ERROR, ${n+(s[(v-20)%10]||s[v]||s[0])} input must be a string actual -> ${inputs[i]}, returning false.`); //eslint-disable-line no-console
-				validateFalse = true;
-			}
-		}
-		if (validateFalse) return false;
+    // function validation
+    let inputs = [un, ipw, pw, iv];
+    let validateFalse = false;
+    for (let i in inputs) {
+      if (typeof inputs[i] !== 'string' || inputs[i] === '') {
+        let n = Number(i) + 1, s = ['th', 'st', 'nd', 'rd'], v = n % 100;
+        console.error(`[RAM ORACLE] ERROR, ${n + (s[(v - 20) % 10] || s[v] || s[0])} input must be a string actual -> ${inputs[i]}, returning false.`); // eslint-disable-line no-console
+        validateFalse = true;
+      }
+    }
+    if (validateFalse) return false;
 
 
-		String.prototype.hexEncode = function () {
-			let result = '';
-			for (let i = 0, l = this.length; i < l; i++) {
-				let hex = this.charCodeAt(i).toString(16);
-				result += ('000' + hex).slice(-2);
-			}
-			return result;
-		};
+    String.prototype.hexEncode = function () {
+      let result = '';
+      for (let i = 0, l = this.length; i < l; i++) {
+        let hex = this.charCodeAt(i).toString(16);
+        result += ('000' + hex).slice(-2);
+      }
+      return result;
+    };
 
-		const su = (un + pw).toUpperCase();
-		const sl = su.length;
-		const ml = ((sl * 2) % 8);
-		const pl = ml === 0 ? 0 : 8 - ml;
+    const su = (un + pw).toUpperCase();
+    const sl = su.length;
+    const ml = ((sl * 2) % 8);
+    const pl = ml === 0 ? 0 : 8 - ml;
 
-		let u = '';
-		for (let i = 0; i < sl; i++) {
-			u += String.fromCharCode(0) + su[i];
-		}
-		u += String.fromCharCode(0, 0, 0, 0, 0, 0, 0, 0).substring(0, pl);
+    let u = '';
+    for (let i = 0; i < sl; i++) {
+      u += String.fromCharCode(0) + su[i];
+    }
+    u += String.fromCharCode(0, 0, 0, 0, 0, 0, 0, 0).substring(0, pl);
 
-		const f = u.hexEncode();
+    const f = u.hexEncode();
 
-		let cipher = crypto.createCipheriv('des-cbc', Buffer.from(iv, 'hex'), Buffer.from('0000000000000000', 'hex'));
-		cipher.setAutoPadding(false);
-		cipher = cipher.update(f, 'hex', 'hex');
-		const sub = cipher.slice(-16);
+    let cipher = crypto.createCipheriv('des-cbc', Buffer.from(iv, 'hex'), Buffer.from('0000000000000000', 'hex'));
+    cipher.setAutoPadding(false);
+    cipher = cipher.update(f, 'hex', 'hex');
+    const sub = cipher.slice(-16);
 
-		let cipher2 = crypto.createCipheriv('des-cbc', Buffer.from(sub, 'hex'), Buffer.from('0000000000000000', 'hex'));
-		cipher2.setAutoPadding(false);
-		cipher2 = cipher2.update(f, 'hex', 'hex');
+    let cipher2 = crypto.createCipheriv('des-cbc', Buffer.from(sub, 'hex'), Buffer.from('0000000000000000', 'hex'));
+    cipher2.setAutoPadding(false);
+    cipher2 = cipher2.update(f, 'hex', 'hex');
 
-		const hash = cipher2.slice(-16).toUpperCase();
+    const hash = cipher2.slice(-16).toUpperCase();
 
-		if (hash === ipw) {
-			return true;
-		} else {
-			return false;
-		}
+    if (hash === ipw) {
+      return true;
+    } else {
+      return false;
+    }
 
-	}
+  }
 
 };
--- test.js
+++ test.js
@@ -1,99 +1,99 @@
 const ram = require('./index.js'),
-	chai = require('chai'),
-	expect = chai.expect;
+  chai = require('chai'),
+  expect = chai.expect;
 
 describe('ram-oracle match function', function () {
 
-	it('determines if password hashes are truthy', function () {
+  it('determines if password hashes are truthy', function () {
 
-		let oracleSchema = 'JDOE',
-			hashedOraclePassword = '587F72032A3C828E',
-			inputPassword = 'password';
-		let matches = ram.match(oracleSchema, hashedOraclePassword, inputPassword);
+    let oracleSchema = 'JDOE',
+      hashedOraclePassword = '587F72032A3C828E',
+      inputPassword = 'password';
+    let matches = ram.match(oracleSchema, hashedOraclePassword, inputPassword);
 
-		expect(matches).to.deep.equal(true);
+    expect(matches).to.deep.equal(true);
 
-	});
+  });
 
-	it('determines if password hashes are truthy for specificying an initialization vector', function () {
+  it('determines if password hashes are truthy for specificying an initialization vector', function () {
 
-		let oracleSchema = 'JDOE',
-			hashedOraclePassword = '587F72032A3C828E',
-			inputPassword = 'password';
-		let matches = ram.match(oracleSchema, hashedOraclePassword, inputPassword, '0123456789ABCDEF' );
+    let oracleSchema = 'JDOE',
+      hashedOraclePassword = '587F72032A3C828E',
+      inputPassword = 'password';
+    let matches = ram.match(oracleSchema, hashedOraclePassword, inputPassword, '0123456789ABCDEF');
 
-		expect(matches).to.deep.equal(true);
+    expect(matches).to.deep.equal(true);
 
-	});
+  });
 
-	it('determines if password hashes are falsey', function () {
+  it('determines if password hashes are falsey', function () {
 
-		let oracleSchema = 'JDOE',
-			hashedOraclePassword = '587F72032A3C828E',
-			inputPassword = 'incorrect_password';
-		let matches = ram.match(oracleSchema, hashedOraclePassword, inputPassword);
+    let oracleSchema = 'JDOE',
+      hashedOraclePassword = '587F72032A3C828E',
+      inputPassword = 'incorrect_password';
+    let matches = ram.match(oracleSchema, hashedOraclePassword, inputPassword);
 
-		expect(matches).to.deep.equal(false);
+    expect(matches).to.deep.equal(false);
 
-	});
+  });
 
-	it('determines if password hashes are falsey for specificying an incorrect initialization vector', function () {
+  it('determines if password hashes are falsey for specificying an incorrect initialization vector', function () {
 
-		let oracleSchema = 'JDOE',
-			hashedOraclePassword = '587F72032A3C828E',
-			inputPassword = 'password';
-		let matches = ram.match(oracleSchema, hashedOraclePassword, inputPassword, '0003456789ABCDEF' );
+    let oracleSchema = 'JDOE',
+      hashedOraclePassword = '587F72032A3C828E',
+      inputPassword = 'password';
+    let matches = ram.match(oracleSchema, hashedOraclePassword, inputPassword, '0003456789ABCDEF');
 
-		expect(matches).to.deep.equal(false);
+    expect(matches).to.deep.equal(false);
 
-	});
+  });
 
-	it('determines validates input for null', function () {
+  it('determines validates input for null', function () {
 
-		let oracleSchema = null,
-			hashedOraclePassword = null,
-			inputPassword = null,
-			iv = null;
-		let matches = ram.match(oracleSchema, hashedOraclePassword, inputPassword, iv);
+    let oracleSchema = null,
+      hashedOraclePassword = null,
+      inputPassword = null,
+      iv = null;
+    let matches = ram.match(oracleSchema, hashedOraclePassword, inputPassword, iv);
 
-		expect(matches).to.deep.equal(false);
+    expect(matches).to.deep.equal(false);
 
-	});
+  });
 
-	it('determines validates input for undefined', function () {
+  it('determines validates input for undefined', function () {
 
-		let oracleSchema,
-			hashedOraclePassword,
-			inputPassword,
-			iv;
-		let matches = ram.match(oracleSchema, hashedOraclePassword, inputPassword, iv);
+    let oracleSchema,
+      hashedOraclePassword,
+      inputPassword,
+      iv;
+    let matches = ram.match(oracleSchema, hashedOraclePassword, inputPassword, iv);
 
-		expect(matches).to.deep.equal(false);
+    expect(matches).to.deep.equal(false);
 
-	});
+  });
 
-	it('determines validates input for empty', function () {
+  it('determines validates input for empty', function () {
 
-		let oracleSchema = '',
-			hashedOraclePassword = '',
-			inputPassword = '',
-			iv = '';
-		let matches = ram.match(oracleSchema, hashedOraclePassword, inputPassword, iv);
+    let oracleSchema = '',
+      hashedOraclePassword = '',
+      inputPassword = '',
+      iv = '';
+    let matches = ram.match(oracleSchema, hashedOraclePassword, inputPassword, iv);
 
-		expect(matches).to.deep.equal(false);
+    expect(matches).to.deep.equal(false);
 
-	});
+  });
 
-	it('determines valid against numbers', function () {
+  it('determines valid against numbers', function () {
 
-		let oracleSchema = 123,
-			hashedOraclePassword = 123,
-			inputPassword = 123,
-			iv = 123;
-		let matches = ram.match(oracleSchema, hashedOraclePassword, inputPassword, iv);
+    let oracleSchema = 123,
+      hashedOraclePassword = 123,
+      inputPassword = 123,
+      iv = 123;
+    let matches = ram.match(oracleSchema, hashedOraclePassword, inputPassword, iv);
 
-		expect(matches).to.deep.equal(false);
+    expect(matches).to.deep.equal(false);
 
-	});
+  });
 
 });
